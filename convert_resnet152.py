79086bb89d1c450d7ea88ccd07033fc6c85ee880
Suo Qiu <q.suo@foxmail.com>
2017-09-21 15:33:33 +0800

    update

--- convert_resnet152.py
+++ convert_resnet152.py
@@ -80,5 +80,5 @@ if __name__ == '__main__':
 
     model_dict = model.state_dict()
     model_dict.update(new_state_dict)
-    model.load_state_dict(new_state_dict)
+    model.load_state_dict(model_dict)
     torch.save(model.state_dict(), 'Places2_365_CNN.pth')
